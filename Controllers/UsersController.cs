334dff7a895ff29ba09e03c1145086ff826004ee
Enric León <enricleonfont@gmail.com>
2018-07-08 19:20:53 +0200

    Link UsersController with UsersRepository

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BudgetApi.Models;
 using Microsoft.AspNetCore.Mvc;
+
+using BudgetApi.Models;
+using BudgetApi.Repositories;
 
 namespace BudgetApi.Controllers
 {
@@ -11,48 +13,49 @@ namespace BudgetApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly UsersRepository _usersRepository;
+        public UsersController()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            _usersRepository = new UsersRepository(connectionString);
+        }
+
         // GET api/users
         [HttpGet]
         public ActionResult<IEnumerable<User>> Get()
         {
-            return new User[] { 
-                new User { 
-                    Id = 1, 
-                    Name = "user1" 
-                }, 
-                new User { 
-                    Id = 2, 
-                    Name = "user2" 
-                }
-            };
+            return _usersRepository.GetAllUsers()?.ToArray();
         }
 
         // GET api/users/5
         [HttpGet("{id}")]
         public ActionResult<User> Get(int id)
         {
-            return new User {
-                Id = 5,
-                Name = "user5"
-            };
+            return _usersRepository.GetUserById(id);
         }
 
         // POST api/users
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<int> Post([FromBody] string name)
         {
+            return _usersRepository.AddNewUser(name);
         }
 
         // PUT api/users/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] string name)
         {
+            User user = _usersRepository.GetUserById(id);
+            user.Name = name;
+
+            _usersRepository.AddNewUser(user);
         }
 
         // DELETE api/users/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _usersRepository.DeleteById(id);
         }
     }
 }
---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using BudgetApi.Models;
+using Marten;
+
+namespace BudgetApi.Repositories
+{
+    public class UsersRepository
+    {
+        private readonly string _connectionString;
+
+        public UsersRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private IDocumentStore Store
+        {
+            get
+            {
+                var store = DocumentStore.For(_ => { _.Connection(_connectionString); });
+                return store;
+            }
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var session = Store.QuerySession())
+            {
+                return session.Query<User>()
+                               .ToArray();
+            }
+        }
+
+        public User GetUserById(int id)
+        {
+            using (var session = Store.QuerySession())
+            {
+                return session.Load<User>(id);
+            }
+        }
+
+        public int AddNewUser(string name)
+        {
+            var newUser = new User { Name = name };
+
+            using (var session = Store.OpenSession())
+            {
+                session.Store(newUser);
+
+                session.SaveChanges();
+            }
+
+            return newUser.Id;
+        }
+
+        public void AddNewUser(User user)
+        {
+            using (var session = Store.OpenSession())
+            {
+                session.Store(user);
+
+                session.SaveChanges();
+            }
+        }
+
+        public void DeleteById(int id)
+        {
+            using (var session = Store.OpenSession())
+            {
+                session.Delete<User>(id);
+
+                session.SaveChanges();
+            }
+        }
+    }
+}
