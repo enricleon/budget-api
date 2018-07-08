44bb89556117da00135b7d35cd145652b22e0dd1
Enric León <enricleonfont@gmail.com>
2018-07-08 19:01:12 +0200

    Change values to users controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BudgetApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return new User[] { 
+                new User { 
+                    Id = 1, 
+                    Name = "user1" 
+                }, 
+                new User { 
+                    Id = 2, 
+                    Name = "user2" 
+                }
+            };
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return new User {
+                Id = 5,
+                Name = "user5"
+            };
+        }
+
+        // POST api/users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
